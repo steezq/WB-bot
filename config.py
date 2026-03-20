6ca0e951bece4550d7854150bd56d69e4abc21c9
steezq <kazakowtsef.max@gmail.com>
2026-03-20 17:20:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,4 +13,4 @@ WB_FINANCE_URL = "https://statistics-api.wildberries.ru/api/v5"
 
 # Allowed users (fill with your Telegram user IDs for security)
 # Leave empty to allow everyone
-ALLOWED_USERS: list[int] = []
+ALLOWED_USERS: list[int] = [1621990225]
